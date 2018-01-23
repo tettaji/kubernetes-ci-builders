f90af5dea2befad6c68aae4bffeb5289473c357b
Tariq Ettaji <tettaji@xebia.com>
2018-01-23 10:58:33 +0100

    Added blog examples

---
+++ jenkins/config.groovy
@@ -0,0 +1,19 @@
+import org.csanchez.jenkins.plugins.kubernetes.*
+import jenkins.model.*
+
+def instance = Jenkins.getInstance()
+def cloudName = 'kubernetes'
+
+// Either create a new config if it doesn't exist or use the existing one
+KubernetesCloud kubernetes = instance.getCloud(cloudName) ?: new KubernetesCloud(cloudName)
+
+kubernetes.setServerUrl("https://kubernetes.default.svc.cluster.local")
+kubernetes.setNamespace("jenkins")
+// Change to your local jenkins URL, note that the format starts with service-name.namespace
+kubernetes.setJenkinsUrl("http://jenkins.default.svc.cluster.local")
+
+if (!instance.getCloud(cloudName)) {
+  instance.clouds.add(kubernetes)
+}
+
+instance.save()
